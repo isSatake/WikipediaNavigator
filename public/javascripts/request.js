abaca633e038ad4ff8acdd216941d60a63055bf0
stkay <doraaki0@gmail.com>
2015-11-15 17:19:41 +0900

    クリックイベント

--- public/javascripts/request.js
+++ public/javascripts/request.js
@@ -1,13 +1,25 @@
 $(function(){
   getCategoryList('君が代');
   getCategoryMembers(encodeURIComponent('Category:1880年代の音楽'));
+
+  $('ul').on('click', 'li', function(){
+    var clicked = $(this);
+    if(clicked.attr('class') == 'music'){
+      getCategoryList(clicked.text());
+    }
+    if(clicked.attr('class') == 'category'){
+      getCategoryMembers(encodeURIComponent(clicked.text()));
+    }
+  });
 });
 
 function getCategoryList(musicTitle){
+  var title = ''
   $('body').append('<ul class="categories"></ul>');
   $.get('categorylist/' + musicTitle, function(categoryList){
     $.each(categoryList, function(index, category){
-      $('ul.categories:last').append('<li class="category">' + category.title + '</li>');
+      title = category.title//.replace(/ategory/, '');
+      $('ul.categories:last').append('<li class="category">' + title + '</li>');
     });
   });
 }
