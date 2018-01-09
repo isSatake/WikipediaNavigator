b531abd0720a650820e83e3413c1d2888ff28e33
stkay <stkayy@gmail.com>
2018-01-09 14:21:23 +0900

    delete favicon

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var debug = require('debug')('server');
 var app = express();
 
 app.set('port', process.env.PORT || '3000');
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
